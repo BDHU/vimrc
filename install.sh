00259bf122be5b86f7a8611ed1691ee87d45749e
Edward Hu <bodunhu@utexas.edu>
2022-11-17 01:43:09 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,9 +5,9 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # Clone the repo
-git clone https://github.com/BDHU/vim_setting.git
+git clone https://github.com/BDHU/vim_setting.git /tmp/
 
-cd vim_setting/Linux/.vimrc ~/
+cp /tmp/vim_setting/Linux/.vimrc ~/
 
 # install all plugins using vimplug
 vim +PlugInstall
