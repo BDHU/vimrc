ec924aa179bf9f93a806b68512d8bf3c1e95b943
Edward Hu <bodunhu@utexas.edu>
2022-11-17 01:41:46 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,15 +5,9 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # Clone the repo
-git clone https://github.com/BDHU/Vim_Setting.git
+git clone https://github.com/BDHU/vim_setting.git
 
-cd Vim_Setting/Linux/
-
-cp .vimrc ~/
-
-# install universal ctag for debian based system
-sudo apt install universal-ctags
-sudo apt install cscope
+cd vim_setting/Linux/.vimrc ~/
 
 # install all plugins using vimplug
 vim +PlugInstall
